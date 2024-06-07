1dc680b1e9a06e5975b8e784ca37cbaef567d4fb
unknown <daimashashank10@gmail.com>
2024-06-08 04:17:19 +0530

    finished

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return 
+  <>
+  <script defer src="https://cloud.umami.is/script.js" data-website-id="bef2b713-e7b6-4037-84ff-847ec76a5eb2"></script>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
