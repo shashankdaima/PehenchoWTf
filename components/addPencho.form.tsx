e7fb1b447511f95f49af409e4a4db5ea1be9d00c
unknown <daimashashank10@gmail.com>
2024-06-08 03:00:01 +0530

    finished

--- components/addPencho.form.tsx
+++ components/addPencho.form.tsx
@@ -46,11 +46,11 @@ const AddPenchoform = (props: AddPenchoformProps) => {
         <div className="flex flex-col gap-y-1" >
             <h2 className="bangers-regular text-3xl">Add Your own Stories</h2>
             <p className="zilla-slab  text-sm">Ensure that your message is respectful and does not contain any personal information or harassment. Protect the anonymity of yourself and your loved ones, as it violates platform policies.</p>
-            <h3 className="zilla-slab  text-lg mt-2 !font-bold">Write your story here</h3>
+            <h3 className="zilla-slab  text-lg mt-2 !font-bold">Write your story here in 100-400 characters</h3>
             <Textarea maxLength={400} minLength={100} value={content} onChange={(e) => { setContent(e.target.value) }} />
             <h3 className="zilla-slab  text-lg mt-2 !font-bold">Make a catchy title...</h3>
             <Input value={title} onChange={(e) => { setTitle(e.target.value) }} />
-            <Button type="submit" disabled={!(content.length > 100&&content.length<400 && title.length > 0)} className="mt-8 md:mt-2 bg-[#2F2F2F] !text-white" onClick={onSubmit}>Submit</Button>
+            <Button type="submit" disabled={!(content.length >= 100&&content.length<=400 && title.length > 0)} className="mt-8 md:mt-2 bg-[#2F2F2F] !text-white" onClick={onSubmit}>Submit</Button>
         </div >
 
     );
