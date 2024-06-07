e8f480e5024d00c8ccc45d39191d6a656bf62371
unknown <daimashashank10@gmail.com>
2024-06-08 04:09:28 +0530

    finished

--- components/LookingforSWE.header.tsx
+++ components/LookingforSWE.header.tsx
@@ -5,7 +5,7 @@ const LookingforSWE_Header = () => {
         <div className="absolute y-0 z-10 bg-[var(--accent-color)]  w-full mx-auto border-0 flex flex-col justify-center items-center border-b-4 border-black">
             <p className="zilla-slab md:text-xl text-lg my-4 text-center mx-4">
                 Are you looking for a fulltime/freelance software engineer?{" "}
-                <Link href="https://shashankdaima.com" className="font-bold hover:underline">
+                <Link href="https://bento.me/shashankdaima" className="font-bold hover:underline">
                     {" "}
                     Get in touch
                     {" ->"}
